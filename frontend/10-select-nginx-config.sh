8effb2bb3f19af9330149e7820b9ff07cc16c0ac
bambooch <amar-cerim@hotmail.com>
2026-05-15 09:33:05 +0200

    ci/cd: nginx https templates fix

--- frontend/10-select-nginx-config.sh
+++ frontend/10-select-nginx-config.sh
@@ -2,9 +2,10 @@
 set -eu
 
 cert_dir="/etc/letsencrypt/live/${PUBLIC_DOMAIN}"
+template_dir="/opt/nginx/templates"
 
 if [ -f "${cert_dir}/fullchain.pem" ] && [ -f "${cert_dir}/privkey.pem" ]; then
-    cp /etc/nginx/templates/https.conf.template /etc/nginx/templates/default.conf.template
+    cp "${template_dir}/https.conf.template" /etc/nginx/templates/default.conf.template
 else
-    cp /etc/nginx/templates/http.conf.template /etc/nginx/templates/default.conf.template
+    cp "${template_dir}/http.conf.template" /etc/nginx/templates/default.conf.template
 fi
