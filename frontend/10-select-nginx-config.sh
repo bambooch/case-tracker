7e91572a02546704a9065d016ba6e4c5318ee789
bambooch <amar-cerim@hotmail.com>
2026-05-15 09:39:21 +0200

    cicd: create nginx template dir

--- frontend/10-select-nginx-config.sh
+++ frontend/10-select-nginx-config.sh
@@ -3,9 +3,12 @@ set -eu
 
 cert_dir="/etc/letsencrypt/live/${PUBLIC_DOMAIN}"
 template_dir="/opt/nginx/templates"
+output_dir="/etc/nginx/templates"
+
+mkdir -p "${output_dir}"
 
 if [ -f "${cert_dir}/fullchain.pem" ] && [ -f "${cert_dir}/privkey.pem" ]; then
-    cp "${template_dir}/https.conf.template" /etc/nginx/templates/default.conf.template
+    cp "${template_dir}/https.conf.template" "${output_dir}/default.conf.template"
 else
-    cp "${template_dir}/http.conf.template" /etc/nginx/templates/default.conf.template
+    cp "${template_dir}/http.conf.template" "${output_dir}/default.conf.template"
 fi
